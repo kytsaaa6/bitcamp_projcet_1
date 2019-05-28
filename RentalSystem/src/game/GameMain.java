b97bb6a58b3f6b70fe7a82e0c09d70ffdde42a4d
이현호 <gobauo8@naver.com>
2019-05-28 16:21:59 +0900

    Game 기능 구현

--- RentalSystem/src/game/GameMain.java
+++ RentalSystem/src/game/GameMain.java
@@ -2,42 +2,38 @@ package game;
 
 import java.util.Scanner;
 
+import util.Util;
+
 public class GameMain {
 
-	public static void main(String[] args) {
-		GameManage gameManage = GameManage.getInstance();
+		public GameManage gameManage = GameManage.getInstance();
 		
+		public void gameMainRun() {
+			
 		
-		while(true) {
-			
-			int choice = gameManage.showMenu();
-			
-			
-			switch(choice){
-			case Util.INSERT:
-				gameManage.insertGame();
-				break;
-			case Util.DELET:
-				gameManage.deletGame();
-				break;
-			case Util.SERCH:
-				gameManage.searchGame();
-				break;
-			case Util.ALLLIST:
-				gameManage.allListGame();
-				break;
-			case Util.QUIT:
-				System.out.println("프로그램을 종료합니다.");
-				return;
-			}
+			while(true) {
 				
-			
-			
-		}
-		
-		
-		
+				int choice = gameManage.showMenu();
+				
+				switch(choice){
+				case Util.INSERT:
+					gameManage.insertGame();
+					break;
+				case Util.DELET:
+					gameManage.deletGame();
+					break;
+				case Util.SERCH:
+					gameManage.searchGame();
+					break;
+				case Util.ALLLIST:
+					gameManage.allListGame();
+					break;
+				case Util.QUIT:
+					System.out.println("프로그램을 종료합니다.");
+					return;
+				}
 
+			}
 	}
 
 }
